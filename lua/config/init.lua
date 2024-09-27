646daf07e6aee13a01d644edeb73f2a819476fb9
msmith-codes <info@msmith.codes>
2024-09-27 01:50:33 -0400

    Added OneDark and custom commands

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,5 +1,5 @@
 return {
-    color_theme = "catppuccin",
+    color_theme = "onedark", -- Can be set to "nord" or "catppuccin"
     color_mode = "dark", -- Can be set to "light" or "dark"
     tree_side = "left", -- Can be set to "left" or "right"
 }
